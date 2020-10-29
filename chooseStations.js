878cb155a81016a9fb203365fc3dd9e4d95a7d31
Ryan MacFarlane <rjl.macfarlane@gmail.com>
2020-10-29 17:13:35 +0000

    Compass exercises

---
+++ chooseStations.js
@@ -0,0 +1,35 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+/*
+function chooseStations(stations) {
+  const goodStations = []
+  for (const station of stations) {
+    const capacity = station[1]
+    if  (capacity >= 20) {
+      const type = station[2]
+      if (type === 'school' || type === 'community centre') {
+        goodStations.push(station[0])
+      }
+    }
+  }
+  return goodStations
+}
+console.log(chooseStations(stations)) 
+*/
+
+// A slight improvement...
+function chooseStations(stations) {
+  const goodStations = []
+  for (const station of stations) {
+    const capacity = station[1]
+    const type = station[2]
+    if  ((capacity >= 20) && (type === 'school' || type === 'community centre')) { 
+      goodStations.push(station[0])
+      }
+    }
+  return goodStations
+}
+console.log(chooseStations(stations)) 
