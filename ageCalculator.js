602fd7b6b48c9c1447bb678f5c41d5da3d49ed3b
Ryan MacFarlane <rjl.macfarlane@gmail.com>
2020-10-29 17:48:06 +0000

    Compass exercises

---
+++ ageCalculator.js
@@ -0,0 +1,6 @@
+const ageCalculator = function(name, yearOfBirth, currentYear) {
+  let age = (currentYear - yearOfBirth)
+  return `${name} is ${age} years old`
+}
+console.log(ageCalculator("Miranda", 1983, 2015));
+console.log(ageCalculator("Ferdinand", 1988, 2015));
