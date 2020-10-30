d29f26fc9f66f3f5623d71b4fedf7b9eae5ae4b9
Ryan MacFarlane <rjl.macfarlane@gmail.com>
2020-10-30 16:00:11 +0000

    Compass exercises

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,21 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+/* 
+
+// Looping through the array using a for loop:
+
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+*/
+
+// Performing the same operation using a while loop:
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
